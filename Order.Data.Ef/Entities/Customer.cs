19deee7e3df40cf30cb03a395d7b9c89c2faf09f
Bill Barge <wbarge@cox.net>
2022-05-12 11:22:39 -0700

    init create
    Created the projects to be used by the application

---
+++ Order.Data.Ef/Entities/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order.Data.Ef.Entities
+{
+    public class Customer
+    {
+        public Guid CustomerId { get;set;}
+        public string CustomerName { get;set;}
+        public string AddressLine1 { get;set;}
+        public string AddressLine2 { get;set;}
+        public string City { get;set;}
+        public string State { get;set;}
+        public string Zip { get;set;}
+        public string Country { get; set;}
+    }
+}
---
+++ Order.Data.Ef/Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order.Data.Ef.Entities
+{
+    public class Product
+    {
+        public Guid ProductId { get;set;}
+        public string ProductName { get;set;}
+        public decimal PricePerItem { get;set;}
+        public int AverageCustomerRating { get;set;}
+    }
+}
